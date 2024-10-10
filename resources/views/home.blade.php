1bbabd51328abc8b4af94850c234d72dd03c0cfb
Komtufity <megachingun@gmail.com>
2024-10-10 08:25:48 +0700

    modified:   .env.example 	new file:   public/img/Kursi-LIDO-Kursi-Plastik-Kursi-Taman-Luar-Ruangan-dengan-Kursi-Sandaran-Tangan-Luar-Ruangan-Polipropilena-Cm.jpg 	new file:   public/img/indomie_indomie-mie-goreng-aceh--90-gr-_full02.webp 	new file:   public/img/rinso_rinso-deterjen-bubuk-molto-deterjen-bubuk--800-g-_full04.webp 	modified:   resources/views/home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
 <head>
-  @vite(['resources/css/app.css','resources/js/app.js'])
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-Marketplace</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-  <!-- Hello World with Flowbite -->
-  <div class="container mx-auto p-4">
-    <h1 class="text-2xl font-bold text-center">Hello, World!</h1>
-    <p class="text-center">This is a simple example of using Flowbite with Laravel and Vite.js</p>
-  </div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">(nama toko)</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Beranda</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Produk</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Kategori</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Keranjang</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Masuk</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container mt-5">
+        <div class="jumbotron text-center">
+            <h1>Selamat datang di (insert nama toko here)</h1>
+            <p class="lead">Temukan produk berkualitas dengan harga terjangkau!</p>
+            <a href="#" class="btn btn-primary">Belanja Sekarang</a>
+        </div>
+
+        <div class="row mt-5">
+            <div class="col-md-4">
+                <div class="card">
+                    <img src="{{ asset('img/rinso_rinso-deterjen-bubuk-molto-deterjen-bubuk--800-g-_full04.webp') }}" class="card-img-top" alt="Product 1">
+                    <div class="card-body">
+                        <h5 class="card-title">Deterjen Rinso Bubuk 800g</h5>
+                        <p class="card-text">Rp. 32.000</p>
+                        <a href="#" class="btn btn-primary">Masukkan ke Keranjang</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <img src="{{ asset('img/indomie_indomie-mie-goreng-aceh--90-gr-_full02.webp') }}" class="card-img-top" alt="Product 2">
+                    <div class="card-body">
+                        <h5 class="card-title">Indomie Goreng Aceh 90g</h5>
+                        <p class="card-text">Rp. 2.500</p>
+                        <a href="#" class="btn btn-primary">Masukkan ke Keranjang</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <img src="{{ asset('img/Kursi-LIDO-Kursi-Plastik-Kursi-Taman-Luar-Ruangan-dengan-Kursi-Sandaran-Tangan-Luar-Ruangan-Polipropilena-Cm.jpg') }}" class="card-img-top" alt="Product 3">
+                    <div class="card-body">
+                        <h5 class="card-title">Kursi LIDO Plastik Putih</h5>
+                        <p class="card-text">Rp. 250.000</p>
+                        <a href="#" class="btn btn-primary">Masukkan ke Keranjang</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <footer class="text-center mt-5">
+        <p>&copy; 2024 (nama toko). All Rights Reserved.</p>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
+</html>
