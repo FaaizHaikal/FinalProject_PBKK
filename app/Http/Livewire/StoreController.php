7d3e30c1b4f66e5698741f78e6b38165a1dfdeb2
FaaizHaikal <faaizhaikal@gmail.com>
2024-10-12 01:52:27 +0700

    feat: add product on store controller

--- app/Http/Livewire/StoreController.php
+++ app/Http/Livewire/StoreController.php
@@ -5,20 +5,33 @@ namespace App\Http\Livewire;
 use Livewire\Attributes\Locked;
 use Livewire\Attributes\Validate;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use App\Models\Product;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Exception;
 
 class StoreController extends Component
 {
 
+        use WithFileUploads;
+
         public $user;
         #[Locked]
         public $isHaveStore = null;
         #[Validate('required')]
+        public $store_name = null;
+        public $products = [];
+
+        // New product fields
+        public $product_name = null;
+        public $product_description = null;
+        public $product_image = null;
+        public $product_stock = null;
+        public $product_price = null;
     
 
         public function mount()
@@ -27,6 +40,8 @@ class StoreController extends Component
             $this->user = User::find(Auth::id());
             if($this->user->store_name != null) {
                 $this->isHaveStore = true;
+                $this->store_name = $this->user->store_name;
+                $this->products = Product::where('user_id', $this->user->id)->get();
             } 
             else {
                 Log::info("dont have duh");
@@ -37,7 +52,6 @@ class StoreController extends Component
         public function AddStore(Request $request)
         {
             $this->user = User::find(Auth::id());
-            Log::info($this->store_name);
 
             if ($this->user && $this->user->store_name == null) {
                 $this->user->store_name = $this->store_name;
@@ -45,22 +59,46 @@ class StoreController extends Component
             }
 
             return redirect('/mystore');
+        }
 
+        public function AddProduct(Request $request)
+        {
+            $this->user = User::find(Auth::id());
+            
+            try {
+                $imagePath = $this->product_image->store('products', 'public'); 
+            } catch (Exception $e) {
+                Log::info($e);
+            }
+
+            try {
+                $newProduct = Product::create([
+                    'name' => $this->product_name,
+                    'description' => $this->product_description,
+                    'image' => $imagePath,
+                    'stock' => $this->product_stock,
+                    'price' => $this->product_price,
+                    'user_id' => $this->user->id,
+                ]);
+
+                $this->reset('product_name', 'product_description', 'product_image', 'product_stock', 'product_price');
+
+                return redirect('/mystore');
+            } catch (Exception $e) {
+                Log::info($e);
+            }
         }
 
         public function render()
         {
-            $user = Auth::user();
+            $user = User::find(Auth::id());
 
             $userId = $user->id;
             $username = $user->username;
             $email = $user->email;
             $store_name = $user->store_name;
+            $products = Product::where('user_id', $userId)->get();
 
-            Log::info($userId);
-            Log::info($username);
-            Log::info($email);
-
-            return view('store', compact('userId', 'username', 'email', 'store_name'))->layout('layouts.app');
+            return view('store', compact('userId', 'username', 'email', 'store_name', 'products'))->layout('layouts.app');
         }
 }
