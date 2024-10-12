d8c2a8a7fae4346232779fa6c873d8edcdefd7ca
Johannes Daniswara Pratama <115395772+kisenaa@users.noreply.github.com>
2024-10-12 09:22:18 +0700

    feat: add some element on auth and carousell slider on home (#13)
    * feat: initial auth
    
    * refactor: fully functional auth with middleware
    
    * add create store
    
    * feat: add carousell image on home and improve auth ui

--- resources/views/Upload.blade.php
+++ resources/views/Upload.blade.php
@@ -1,24 +1,36 @@
 @section('title', 'Shop - Upload File Testing')
 
 <div class="h-screen w-full">
-        <div class="flex items-center justify-center h-full">
-            <div class>
+        <div class="flex h-full flex-col items-center justify-center pb-[15%]">
+            <div class="">
                 <form wire:submit="save">
-                    <input class="block w-[300px] text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" 
+                    <input class="block cursor-pointer rounded-lg border border-gray-300 bg-gray-50 text-sm text-gray-900 focus:outline-none lg:w-96 xl:w-[32rem]" 
                         id="file_input" 
                         type="file" 
-                        accept=".svg, .png, .jpg, .jpeg, .gif"        
+                        accept=".svg, .png, .jpg, .jpeg, .gif."        
                         wire:model="photo" 
                     >   
-                    <p class="mt-1 ml-1 text-sm text-gray-500 dark:text-gray-300" id="file_input_help">SVG, PNG, JPG, PNEG, or GIF</p>
-                    <button class="block mt-3 ml-1 text-sm font-medium text-gray-900 dark:text-white" type="submit">Save Photo</button>
-                    @error('photo') <span class="error">{{ $message }}</span> @enderror
+                    <p class="ml-1 mt-1 text-sm text-gray-500 dark:text-gray-300" id="file_input_help">SVG, PNG, JPG, JPEG, or GIF</p>
+                    <div class="item-center flex justify-center">
+                        <button type="submit" class="mt-7 w-[40%] items-center rounded-lg bg-gradient-to-r from-purple-500 to-pink-500 py-2 text-sm font-semibold text-white hover:bg-gradient-to-l focus:outline-none focus:ring-4 focus:ring-purple-200 dark:focus:ring-purple-800">Start Upload</button>
+                    </div>
+
+                    @error('photo') <span class="error mt-2 text-sm text-red-600">{{ $message }}</span> @enderror
                 </form>
             </div>
 
             @if ($fileUrl)
-                <p class="mt-6 text-sm text-gray-500 dark:text-gray-300">Uploaded file:</p>
-                <img src="{{ asset('/storage' . '/z' . $fileUrl) }}" class="mt-2 w-32 h-32 object-cover" alt="Uploaded Image">
+            <div class="flex flex-row gap-6">
+                <div>
+                    <div class="mt-6 text-center text-sm text-gray-500 dark:text-gray-300">Your File:</div>
+                    <img src="{{ asset('/storage' . '/' . $fileUrl) }}" class="mt-2 h-32 w-32 object-cover" alt="Uploaded Image">
+                </div>
+                <div>
+                    <div class="mt-6 text-center text-sm text-gray-500 dark:text-gray-300">Response File:</div>
+                    <img src="{{ asset('/storage' . '/' . $fileUrl) }}" class="mt-2 h-32 w-32 object-cover" alt="Response Image">
+                </div>
+            </div>
             @endif
         </div>
+        
 </div>
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -86,6 +86,15 @@
                     <button type="submit"
                         class="w-full rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300">Submit</button>
                 </form>
+                
+                <div class="mt-4 ml-1 text-xs font-semibold item-center text-center">
+                    <span class="text-gray-600">
+                        Don't have an account ?
+                    </span>
+                    <a class="text-blue-600 hover:underline" href="/auth/register">
+                       Sign up here
+                    </a>
+                </div>
 
             </div>
         </div>
